60f07be9318b44c382005ad980a77829995a92d8
Karim Alibhai <k4rim.sa@gmail.com>
2015-06-18 01:03:57 -0400

    beautify

--- index.js
+++ index.js
@@ -9,152 +9,159 @@
 'use strict';
 
 var es = require('event-stream'),
-	natural = require('natural'),
-	EventEmitter = require('eventemitter2').EventEmitter2,
+    natural = require('natural'),
+    EventEmitter = require('eventemitter2').EventEmitter2,
 
-	/**
-	 * THE HELPERS.
-	 */
-	wrap = function (fn, ctx) {
-		return function () {
-			return fn.apply(ctx, arguments) || ctx;
-		};
-	},
-	stemmer = {
-		stem: natural.PorterStemmer.stem.bind(natural.PorterStemmer),
-		tokenize: natural.PorterStemmer.tokenizeAndStem.bind(natural.PorterStemmer),
-		clean: function (str, filter) {
-			// filter isn't required, just allow
-			// everything if none is given
-			filter = filter || function () {
-				return true;
-			};
-			
-			// tokenize and stem the way we like to do it
-			return str
-					// clean up any punctuation
-					.replace(/[\.,]+/g, '')
-					
-					// split by a space rather than the
-					// traditional aggressive '\W'
-					.split(/\s+/g)
-					
-					// individually stem every word
-					.map(function (word, index) {
-						var original = word;
-						if (filter(word)) word = stemmer.stem(word);
-						
-						// create object to maintain original
-						return {
-							original: original,
-							
-							toString: function () {
-								return word;
-							},
-							
-							valueOf: function () {
-								return word;
-							}
-						};
-					})
-					
-					// filter all stopwords
-					.filter(function (word) {
-						return natural.stopwords.indexOf(String(word)) === -1;
-					});
-		}
-	},
-	
-	/**
-	 * THE TRANSFORMS.
-	 * Built-in transforms to parse out things like
-	 * variables and apply filters.
-	 */
-	transforms = [
-		// VARIABLE TRANSFORM.
-		// parse the respective variables out of the input
-		// and into the data object
-		function (data, next) {
-			var prompt = stemmer.clean(data.prompt, function (word) {
-					return word[0] !== '$';
-				}),
-				input = stemmer.clean(data.input);
-			
-			for (var i = 0; i < prompt.length; i += 1) {
-				if (String(prompt[i])[0] === '$') {
-					data.data[String(prompt[i]).substr(1)] = input[0].original;
-				} else input = input.slice(1);
-			}
-			
-			next(null, data);
-		},
-		
-		// FILTER TRANSFORM.
-		// apply the respective filters to every variable
-		function (data, next) {
-			next(null, data);
-		}
-	],
-	
-	/**
-	 * THE PROTOTYPE.
-	 */
-	alfred = {
-		/**
-		 * adds a new command handler.
-		 * @param {String|Array} prompts - a single string or an array of strings of alfred commands.
-		 * @param {Function|Generator} handler - a function or ES6 generator to handle the command. 
-		 **/
-		add: function ( prompts , handler ) {
-			// string is permitted, but we want to work with
-			// an array of strings at the end of the day.
-			prompts = (prompts instanceof Array ? prompts : [prompts]).filter(function (prompt) {
-				return prompt && typeof prompt === 'string';
-			});
-			
-			// get the type of the handler that will be used
-			// for these prompts, and create the respective command
-			// object
-			var n = this._commands.push({
-				prompts: prompts,
-				handler: handler,
-				htype: handler.constructor.name === 'GeneratorFunction' ? 'generator' : 'function',
-				
-				// create a new classifier for to identify strictly
-				// between the different prompts
-				classifier: new natural.BayesClassifier()
-			}) - 1;
-			
-			// prepare both the inner classifier as well as
-			// alfred's general classifier to be prepared for
-			// both steps of command execution
-			for (var prompt of prompts) {
-				this._commands[n].classifier.addDocument(prompt, prompt);
-				this._classifier.addDocument(prompt, String(n));
-			}
-			
-			// re-train the alfred classifier and the command's
-			// inner classifier
-			this._commands[n].classifier.train();
-			this._classifier.train();
-		},
-		
-		/**
-		 * add a transform to the input processing stream
-		 * @params {Function} transform - a map-stream function to apply.
-		 */
-		use: function ( transform ) {
-			this._tpipe = this._tpipe.pipe(es.map(transform));
-		},
-		
-		/**
-		 * pass a string down alfred as output
-		 * @params {String} output - the output to produce
-		 */
-		say: function ( output ) {
-			this.write('\0' + String(output));
-		}
-	};
+    /**
+     * THE HELPERS.
+     */
+    wrap = function (fn, ctx) {
+        return function () {
+            return fn.apply(ctx, arguments) || ctx;
+        };
+    },
+    stemmer = {
+        stem: natural.PorterStemmer.stem.bind(natural.PorterStemmer),
+        tokenize: natural.PorterStemmer.tokenizeAndStem.bind(natural.PorterStemmer),
+        clean: function (str, filter) {
+            // filter isn't required, just allow
+            // everything if none is given
+            filter = filter || function () {
+                return true;
+            };
+
+            // tokenize and stem the way we like to do it
+            return str
+                // clean up any punctuation
+                .replace(/[\.,]+/g, '')
+
+            // split by a space rather than the
+            // traditional aggressive '\W'
+            .split(/\s+/g)
+
+            // individually stem every word
+            .map(function (word, index) {
+                var original = word;
+
+                // if filter allows it, then 
+                // stemmify
+                if (filter(word)) {
+                    word = stemmer.stem(word);
+                }
+
+                // create object to maintain original
+                return {
+                    original: original,
+
+                    toString: function () {
+                        return word;
+                    },
+
+                    valueOf: function () {
+                        return word;
+                    }
+                };
+            })
+
+            // filter all stopwords
+            .filter(function (word) {
+                return natural.stopwords.indexOf(String(word)) === -1 && natural.stopwords.indexOf(word.original) === -1;
+            });
+        }
+    },
+
+    /**
+     * THE TRANSFORMS.
+     * Built-in transforms to parse out things like
+     * variables and apply filters.
+     */
+    transforms = [
+        // VARIABLE TRANSFORM.
+        // parse the respective variables out of the input
+        // and into the data object
+        function (data, next) {
+            var prompt = stemmer.clean(data.prompt, function (word) {
+                    return word[0] !== '$';
+                }),
+                input = stemmer.clean(data.input);
+
+            for (var i = 0; i < prompt.length; i += 1) {
+                if (String(prompt[i])[0] === '$') {
+                    data.data[String(prompt[i]).substr(1)] = input[0].original;
+                } else {
+                    input = input.slice(1);
+                }
+            }
+
+            next(null, data);
+        },
+
+        // FILTER TRANSFORM.
+        // apply the respective filters to every variable
+        function (data, next) {
+            next(null, data);
+        }
+    ],
+
+    /**
+     * THE PROTOTYPE.
+     */
+    alfred = {
+        /**
+         * adds a new command handler.
+         * @param {String|Array} prompts - a single string or an array of strings of alfred commands.
+         * @param {Function|Generator} handler - a function or ES6 generator to handle the command. 
+         **/
+        add: function (prompts, handler) {
+            // string is permitted, but we want to work with
+            // an array of strings at the end of the day.
+            prompts = (prompts instanceof Array ? prompts : [prompts]).filter(function (prompt) {
+                return prompt && typeof prompt === 'string';
+            });
+
+            // get the type of the handler that will be used
+            // for these prompts, and create the respective command
+            // object
+            var n = this._commands.push({
+                prompts: prompts,
+                handler: handler,
+                htype: handler.constructor.name === 'GeneratorFunction' ? 'generator' : 'function',
+
+                // create a new classifier for to identify strictly
+                // between the different prompts
+                classifier: new natural.BayesClassifier()
+            }) - 1;
+
+            // prepare both the inner classifier as well as
+            // alfred's general classifier to be prepared for
+            // both steps of command execution
+            for (var prompt of prompts) {
+                this._commands[n].classifier.addDocument(prompt, prompt);
+                this._classifier.addDocument(prompt, String(n));
+            }
+
+            // re-train the alfred classifier and the command's
+            // inner classifier
+            this._commands[n].classifier.train();
+            this._classifier.train();
+        },
+
+        /**
+         * add a transform to the input processing stream
+         * @params {Function} transform - a map-stream function to apply.
+         */
+        use: function (transform) {
+            this._tpipe = this._tpipe.pipe(es.map(transform));
+        },
+
+        /**
+         * pass a string down alfred as output
+         * @params {String} output - the output to produce
+         */
+        say: function (output) {
+            this.write('\0' + String(output));
+        }
+    };
 
 /**
  * THE MODULE.
@@ -163,152 +170,174 @@ var es = require('event-stream'),
  * alfred that have no inner relation.
  */
 module.exports = function () {
-	var stream = es.pause().pipe(es.map(function (data, next) {
-		// pause the stream, because we only want to handle
-		// one piece of information at a time.
-		stream.pause();
-		
-		// replace next with a stream resumer as well
-		// so we don't forget to resume the stream
-		next = (function (then) {
-			return function (err, dat) {
-				then(err, dat);
-				if (err || dat) stream.resume();
-			};
-		}(next));
-		
-		// we only want strings as our data
-		data = String(data);
-		
-		// ignore data packets that begin with a null byte
-		// -> pass it off as output
-		if (data[0] === '\0') return next(null, data.substr(1));
-		
-		// if input is needed, forward it to the event emitter
-		// and drop inside the stream
-		if (stream._needsinput) {
-			// emit the data
-			stream._input.write(data);
-			
-			// drop data
-			return next();
-		}
-		
-		if (!stream._needsinput) {
-			// identify the best command handler which will then
-			// handle this data packet
-			var classifications = stream._classifier.getClassifications(data);
-			
-			// add an empty classification in case
-			// no commands have been defined yet
-			classifications.push({
-				label: null,
-				value: 0
-			});
-			
-			// we do two 'confidence' checks to make sure the
-			// input isn't some random thing we are not prepared to
-			// encounter
-			if (classifications[0].value <= 1 / classifications.length) {
-				// swap out data for 'default', to handle
-				// the unrecognized data input
-				// ...
-				return next();
-			}
-			
-			// grab the appropriate command object
-			var command = stream._commands[parseInt(classifications[0].label, 10)];
-			
-			// now within the command object, re-classify our input
-			// via the inner classifier to get a more appropriate prompt
-			// styler
-			var prompt = command.classifier.classify(data);
-			
-			// pass an object containing relevant data through our
-			// transform, and use its final output as our input
-			stream._transform.once('data', function (transformed) {
-				if (command.htype === 'generator') {
-					// grab the handler for the generator
-					var handler = command.handler.apply(stream, [prompt, transformed.data]),
-					
-						// fire off the handler to run until it yields
-						// a prompt
-						tmp = handler.next();
-						
-					// continue onwards
-					stream._needsinput = true;
-					if (tmp.value) next(null, tmp.value);
-					else next();
-					
-					// iterate until we can't
-					var iterate = function () {
-						if (!tmp.done) {
-							// get input
-							stream._input.once('data', function (input) {
-								stream._input.pause();
-								
-								// provide input on return
-								tmp = handler.next(input);
-								
-								// use output for alfred
-								if (tmp.value) stream.write('\0' + tmp.value);
-								
-								// and again
-								iterate();
-							});
-							
-							stream._input.resume();
-						} else stream._needsinput = false;
-					};
-					
-					iterate();
-				} else {
-					// simply execute the function
-					var output = command.handler.apply(stream, [prompt, transformed.data]);
-					
-					// use any output as alfred's output
-					if (output) next(null, output);
-					
-					// otherwise drop the data
-					else next();
-				}
-			}).write({
-				prompt: prompt,
-				input: data,
-				data: {}
-			});
-		} else {
-			// drop anything else
-			next();
-		}
-	}));
-	
-	// copy all of alfred's methods onto the stream
-	for (var key in alfred) {
-		stream[key] = wrap(alfred[key], stream);
-	}
-	
-	// some needed properties
-	stream._input = es.pause();
-	stream._input.pause();
-	stream._commands = [];
-	stream._needsinput = false;
-	stream._classifier = new natural.LogisticRegressionClassifier();
-	stream._transform = es.pause();
-	stream._tpipe = stream._transform;
-	
-	// built-in filters
-	stream._filters = {
-		'string': String,
-		'float': parseFloat,
-		'int': function (data) {
-			return parseInt(data, 10);
-		}
-	};
-	
-	// apply all transforms in order
-	for (var transform of transforms) stream.use(transform);
-	
-	// return the final stream
-	return stream;
+    var stream = es.pause().pipe(es.map(function (data, next) {
+        // pause the stream, because we only want to handle
+        // one piece of information at a time.
+        stream.pause();
+
+        // replace next with a stream resumer as well
+        // so we don't forget to resume the stream
+        next = (function (then) {
+            return function (err, dat) {
+                then(err, dat);
+
+                // only resume the stream if
+                // the data was not dropped
+                if (err || dat) {
+                    stream.resume();
+                }
+            };
+        }(next));
+
+        // we only want strings as our data
+        data = String(data);
+
+        // ignore data packets that begin with a null byte
+        // -> pass it off as output
+        if (data[0] === '\0') {
+            return next(null, data.substr(1));
+        }
+
+        // if input is needed, forward it to the event emitter
+        // and drop inside the stream
+        if (stream._needsinput) {
+            // emit the data
+            stream._input.write(data);
+
+            // drop data
+            return next();
+        }
+
+        if (!stream._needsinput) {
+            // identify the best command handler which will then
+            // handle this data packet
+            var classifications = stream._classifier.getClassifications(data);
+
+            // add an empty classification in case
+            // no commands have been defined yet
+            classifications.push({
+                label: null,
+                value: 0
+            });
+
+            // we do two 'confidence' checks to make sure the
+            // input isn't some random thing we are not prepared to
+            // encounter
+            if (classifications[0].value <= 1 / classifications.length) {
+                // swap out data for 'default', to handle
+                // the unrecognized data input
+                // ...
+                return next();
+            }
+
+            // grab the appropriate command object
+            var command = stream._commands[parseInt(classifications[0].label, 10)];
+
+            // now within the command object, re-classify our input
+            // via the inner classifier to get a more appropriate prompt
+            // styler
+            var prompt = command.classifier.classify(data);
+
+            // pass an object containing relevant data through our
+            // transform, and use its final output as our input
+            stream._transform.once('data', function (transformed) {
+                if (command.htype === 'generator') {
+                    // grab the handler for the generator
+                    var handler = command.handler.apply(stream, [prompt, transformed.data]),
+
+                        // fire off the handler to run until it yields
+                        // a prompt
+                        tmp = handler.next();
+
+                    // continue onwards
+                    stream._needsinput = true;
+                    if (tmp.value) {
+                        next(null, tmp.value);
+                    } else {
+                        next();
+                    }
+
+                    // iterate until we can't
+                    var iterate = function () {
+                        if (!tmp.done) {
+                            // get input
+                            stream._input.once('data', function (input) {
+                                stream._input.pause();
+
+                                // provide input on return
+                                tmp = handler.next(input);
+
+                                // use output for alfred
+                                if (tmp.value) {
+                                    stream.write('\0' + tmp.value);
+                                }
+
+                                // and again
+                                iterate();
+                            });
+
+                            stream._input.resume();
+                        } else {
+                            stream._needsinput = false;
+                        }
+                    };
+
+                    iterate();
+                } else {
+                    // simply execute the function
+                    var output = command.handler.apply(stream, [prompt, transformed.data]);
+
+                    // use any output as alfred's output
+                    if (output) {
+                        next(null, output);
+                    }
+
+                    // otherwise drop the data
+                    else {
+                        next();
+                    }
+                }
+            }).write({
+                prompt: prompt,
+                input: data,
+                data: {}
+            });
+        } else {
+            // drop anything else
+            next();
+        }
+    }));
+
+    // copy all of alfred's methods onto the stream
+    for (var key in alfred) {
+        if (alfred.hasOwnProperty(key)) {
+            stream[key] = wrap(alfred[key], stream);
+        }
+    }
+
+    // some needed properties
+    stream._input = es.pause();
+    stream._input.pause();
+    stream._commands = [];
+    stream._needsinput = false;
+    stream._classifier = new natural.LogisticRegressionClassifier();
+    stream._transform = es.pause();
+    stream._tpipe = stream._transform;
+
+    // built-in filters
+    stream._filters = {
+        'string': String,
+        'float': parseFloat,
+        'int': function (data) {
+            return parseInt(data, 10);
+        }
+    };
+
+    // apply all transforms in order
+    for (var transform of transforms) {
+        stream.use(transform);
+    }
+
+    // return the final stream
+    return stream;
 };
--- test/test-alfred.js
+++ test/test-alfred.js
@@ -5,78 +5,81 @@
  * Licensed under GPLv3.
  * Copyright (C) 2015 Karim Alibhai.
  */
+/*globals beforeEach, describe, it*/
 
-var should = require ( 'should' ),
-	mocha = require ( 'mocha' ),
-	alfred;
-	
+'use strict';
+
+var should = require('should'),
+    mocha = require('mocha'),
+    alfred;
+
 // use a new instance of alfred each time
 beforeEach(function () {
-	alfred = require('../')();
+    alfred = require('../')();
 });
-	
+
 // simple hello world test
 describe('test simple hello world command', () => {
-	describe('use a function', () => {
-		it('should produce output in the stream', (done) => {
-			// define our command
-			alfred.add('hello, *.', () => {
-				return 'hello, world.';
-			});
-	
-			// prepare to test output
-			alfred.on('data', (chunk) => {
-				chunk.should.equal('hello, world.');
-				done();
-			});
-	
-			// write some input
-			alfred.write('hello, alfred.');
-		});
-	});
-	
-	
-	describe('use a generator', () => {
-		it('should produce output in the stream', (done) => {
-			// define our command
-			alfred.add('hello, *.', function* () {
-				(yield 'is this a test?').should.equal('yes, this is a test.');
-				return 'hello, world.';
-			});
-	
-			// prepare to test output
-			alfred.once('data', (chunk) => {
-				chunk.should.equal('is this a test?');
-				alfred.once('data', (chunk) => {
-					chunk.should.equal('hello, world.');
-					done();
-				}).write('yes, this is a test.');
-			});
-	
-			// write some input
-			alfred.write('hello, alfred.');
-		});
-	});
+    describe('use a function', () => {
+        it('should produce output in the stream', (done) => {
+            // define our command
+            alfred.add('hello, *.', () => {
+                return 'hello, world.';
+            });
+
+            // prepare to test output
+            alfred.on('data', (chunk) => {
+                chunk.should.equal('hello, world.');
+                done();
+            });
+
+            // write some input
+            alfred.write('hello, alfred.');
+        });
+    });
+
+
+    describe('use a generator', () => {
+        it('should produce output in the stream', (done) => {
+            // define our command
+            alfred.add('hello, *.', function* () {
+                (yield 'is this a test?').should.equal('yes, this is a test.');
+                return 'hello, world.';
+            });
+
+            // prepare to test output
+            alfred.once('data', (chunk) => {
+                chunk.should.equal('is this a test?');
+                alfred.once('data', (chunk) => {
+                    chunk.should.equal('hello, world.');
+                    done();
+                }).write('yes, this is a test.');
+            });
+
+            // write some input
+            alfred.write('hello, alfred.');
+        });
+    });
 });
 
 // test variables
 describe('test out variables', function () {
-	describe('simple eol variable', function () {
-		it('should grab the rest of the string', function (done) {
-			alfred.add('hello, $who.', function (input, data) {
-				data.should.be.eql({
-					who: 'alfred'
-				});
-				
-				this.say('hello, master.');
-			});
-			
-			alfred.on('data', function (chunk) {
-				chunk.should.equal('hello, master.');
-				done();
-			});
-			
-			alfred.write('hello, alfred.');
-		});
-	});
+    describe('simple eol variable', function () {
+        it('should grab the rest of the string', function (done) {
+            alfred.add('hello, $who.', function (input, data) {
+                data.should.be.eql({
+                    who: 'alfred'
+                });
+
+                this.say('hello, master.');
+            });
+
+            alfred.on('data', function (chunk) {
+                chunk.should.equal('hello, master.');
+                done();
+            });
+
+            alfred.write('hello, alfred.');
+        });
+    });
 });
